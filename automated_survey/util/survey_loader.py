b61d5d0befcfd1a7fea2cf404d990896e79fb819
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-21 18:07:33 -0500

    Remove catching KeyError

--- automated_survey/util/survey_loader.py
+++ automated_survey/util/survey_loader.py
@@ -8,13 +8,9 @@ class SurveyLoader(object):
         self.survey_content = survey_content
 
     def load_survey(self):
-        try:
-            survey = json.loads(self.survey_content)
-            new_survey = Survey(title=survey['title'])
-            questions = map(lambda q: Question(body=q['body'], kind=q['kind']), survey['questions'])
+        survey = json.loads(self.survey_content)
+        new_survey = Survey(title=survey['title'])
+        questions = map(lambda q: Question(body=q['body'], kind=q['kind']), survey['questions'])
 
-            new_survey.save()
-            new_survey.question_set.add(*questions)
-
-        except KeyError:
-            print('Malformed JSON file for survey')
+        new_survey.save()
+        new_survey.question_set.add(*questions)
