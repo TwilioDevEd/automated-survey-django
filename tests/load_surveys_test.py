68aa494e1f22fa8b786390b899d05ccaa941878d
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-21 17:59:04 -0500

    Unit to integration test

--- tests/load_surveys_test.py
+++ tests/load_surveys_test.py
@@ -1,22 +1,15 @@
-import pytest
-
 from automated_survey.models import Survey, Question
-from automated_survey.util.survey_loader import SurveyLoader
 from django.test import TestCase
-
-sample_survey = """
-{"title": "About bears", "questions": [{"body": "What type of bear is best?", "kind": "voice"}, {"body": "In a scale of 1 to 10 how cute do you find koalas?", "kind": "numeric"}, {"body": "Do you think bears beat beets?", "kind": "yes-no"}]}
-"""
+from django.core.management import call_command
 
 
 class SurveyLoaderTest(TestCase):
     def test_load_survey(self):
-        loader = SurveyLoader(sample_survey)
-        loader.load_survey()
+        call_command('load_survey', 'tests/fixtures/bear_survey.json')
 
         all_surveys = Survey.objects.all()
         all_questions = Question.objects.all()
 
         assert len(all_surveys) == 1
-        assert len(all_questions) == 3
+        assert len(all_questions) == 5
         assert all_surveys.first().title == 'About bears'
