3a93e34f4b92693038ff67dad0a5660826ecb2f0
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-24 12:34:22 -0500

    Remove useless variables

--- automated_survey/settings/common.py
+++ automated_survey/settings/common.py
@@ -3,8 +3,6 @@ import os
 from .env import env
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(__file__)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -92,7 +90,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = 'static/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
