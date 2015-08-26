e9602e8c25a1160bdf28d270ed98f59b63de4fb8
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-26 17:11:55 -0500

    Add trailing slash in redirect

--- twilio_sample_project/urls.py
+++ twilio_sample_project/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^automated-survey/', include('automated_survey.urls'), name='surveys'),
-    url(r'^$', lambda r: redirect('/automated-survey'), name='root-redirect')
+    url(r'^$', lambda r: redirect('/automated-survey/'), name='root-redirect')
 ]
