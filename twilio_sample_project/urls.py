4d85450d31d71b18bf358f82f4c62dac1f1952cb
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-26 15:14:31 -0500

    Add redirect so / isn't an error

--- twilio_sample_project/urls.py
+++ twilio_sample_project/urls.py
@@ -1,5 +1,7 @@
+from django.shortcuts import redirect
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^automated_survey/', include('automated_survey.urls'))
+    url(r'^automated_survey/', include('automated_survey.urls'), name='surveys'),
+    url(r'^$', lambda r: redirect('/automated_survey'), name='root-redirect')
 ]
