8f0a88e5aa765c78514afa972ac2f15c81b323a8
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-08-26 16:16:42 -0500

    Do not run data migrations while testing

--- automated_survey/migrations/0003_auto_20150826_2050.py
+++ automated_survey/migrations/0003_auto_20150826_2050.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+
 from django.db import migrations
 from django.core.management import call_command
 
 
 def load_sample_survey(apps, schema_editor):
-    call_command('load_survey', 'automated_survey/tests/fixtures/bear_survey.json')
+    if os.environ.get('DJANGO_SETTINGS_MODULE') == 'twilio_sample_project.settings.test':
+        return
+    else:
+        call_command('load_survey', 'automated_survey/tests/fixtures/bear_survey.json')
 
 
 class Migration(migrations.Migration):
