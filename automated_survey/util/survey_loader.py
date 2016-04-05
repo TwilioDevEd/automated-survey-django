716dcfe2274ba36a9303b5fd308bb0b1d5a145b9
Victor Shyba <victor.shyba@gmail.com>
2016-04-05 11:49:01 -0300

    Turns a map into a list comprehension
    For the sake of simplicity, list comprehension reads better on a
    tutorial.

--- automated_survey/util/survey_loader.py
+++ automated_survey/util/survey_loader.py
@@ -5,16 +5,12 @@ from automated_survey.models import Survey, Question
 class SurveyLoader(object):
 
     def __init__(self, survey_content):
-        self.survey_content = survey_content
+        self.survey = json.loads(survey_content)
 
     def load_survey(self):
-        survey = json.loads(self.survey_content)
-        new_survey = Survey(title=survey['title'])
-        questions = map(
-            lambda q: Question(
-                body=q['body'],
-                kind=q['kind']),
-            survey['questions'])
-
+        new_survey = Survey(title=self.survey['title'])
+        questions = [Question(body=question['body'],
+                              kind=question['kind'])
+                     for question in self.survey['questions']]
         new_survey.save()
         new_survey.question_set.add(*questions)
